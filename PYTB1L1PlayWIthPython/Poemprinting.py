7859558773dcea7c60e965be5ba8bf60d7068540
Rafboerhout <30835@ma-web.nl>
2020-10-07 11:46:59 +0200

    first commit

---
+++ PYTB1L1PlayWIthPython/Poemprinting.py
@@ -0,0 +1,17 @@
+vers1 = "Ik ben Rafboerhout"
+ver2 =  "Na mijn opleiding ga ik voor goud"
+vers3 = "Ik werk nu als bezorger"
+vers4 = "Mijn moeder werkt als dierenverzorger"
+vers5 = "Ik zit nu op het MA"
+vers6 = "Dat is waar ik elke dag naar toe ga"
+vers7 = "Mijn opleiding is software developer"
+vers8 = "Soms heb ik een vastloper"
+vers9 = "We werken met python"
+vers10 = "Ik wist niet dat ik dat kon"
+vers11 = "Ik hoop dat het een heel leuk jaar word"
+vers12 = "Ik zit hier nog 4 jaar"
+vers13 = "Dus hoopelijk ben ik snel klaar"
+vers14 = "dit is het einde van mijn gedicht"
+vers15 = "Ik ben blij kijk maar naar mijn gezicht"
+
+print(vers1,ver2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10,vers11,vers12,vers12,vers13,vers14,vers15)
---
+++ PYTB1L2intestingTellMore/python opdracht.py
@@ -0,0 +1,49 @@
+# Doel: Vragen beatwoorden door de computer.
+
+# intput()
+# print()
+#variabelen
+
+print("Wat is jou naam?")
+naam = input()
+print(naam)
+
+print( "Wat is je achternaam?")
+anaam = input()
+print(anaam)
+
+print("Jou volledige naam is dus:")
+print ( naam + anaam)
+
+print("Waar woon je?")
+Woonplaats = input()
+print("Leuk dus je woont in") 
+print(Woonplaats)
+
+print("Werk je al")
+answer = input("zeg ja of nee : ")
+if answer == "ja":
+    print("Leuk waar werk je dan?")
+    werk = input()
+    print("leuk, ik werk bij new york pizza")
+    
+
+elif answer == "nee":
+    print("wil je ergens werken?")
+    antwoord = input("zeg ja of nee : ")
+    
+    if antwoord == "ja":
+        print("Waar wil je werken?")
+        werK = input()
+        print("leuk, ik werk bij new york pizza")
+        
+    elif antwoord == "nee":
+        print("oke maakt niet uit")
+
+
+print("")
+antwoord = input()
+print(antwoord)
+
+
+    
